8911212d32a4e573e2840f9e9cbae040b06c978f
theliolife <theliolife@theliolifedeMacBook-Air.local>
2015-09-28 12:01:57 +0800

    templates

--- Lio/blog/views.py
+++ Lio/blog/views.py
@@ -1,12 +1,20 @@
+# -*- coding: UTF-8 -*-
 from django.shortcuts import render
+
 from django.http import HttpResponse
+from django.template import loader,Context
+
 # Create your views here.
 
 def show(req):
     return HttpResponse('<html>123</html>')
 
 def index(req):
-    return HttpResponse('<html>234</html>')
+    t = loader.get_template('blog/index.html')
+    
+    tab_list = ['内容主题','文章质量','地理位置','其他']
+    c = Context({'tab_list':tab_list})
+    return HttpResponse(t.render(c)) 
 
 def indexnum(req, num):
     return HttpResponse('<html>345</html>')
